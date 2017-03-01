325948584c8bf77a7067848742c874f0ee13a725
Wassili Hense <hense.wassili@googlemail.com>
2017-03-01 16:13:03 +0100

    REF

--- Server/Properties/VersionInfo.cs
+++ Server/Properties/VersionInfo.cs
@@ -17,5 +17,5 @@ using System.Reflection;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.4.1703.1015")]
-[assembly: AssemblyFileVersion("0.4.1703.1015")]
+[assembly: AssemblyVersion("0.4.1703.1019")]
+[assembly: AssemblyFileVersion("0.4.1703.1019")]
